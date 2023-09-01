e2c6e8474284d7b5da2c715b0ab4093341bbcd5c
Sonu Raut <sonu16122001@gmail.com>
2023-09-01 10:15:28 +0530

    added basic template for tooltip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import Button from "./Button";
 function App() {
+  const [content, setContent] = useState("");
+  const [position, setPosition] = useState("top");
+  const [formSubmit, setFormSubmit] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormSubmit(true);
+    console.log("content : ", content);
+    console.log("position : ", position);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-container">
+        <form id="tooltip-prop-form" onSubmit={handleSubmit}>
+          <label>Content for Tool Tip: </label>
+          <input
+            type="text"
+            name="content"
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <label>Position: </label>
+          <select
+            defaultValue={"top"}
+            onChange={(e) => setPosition(e.target.value)}
+          >
+            <option value={"top"}>top</option>
+            <option value={"left"}>left</option>
+            <option value={"right"}>right</option>
+            <option value={"bottom"}>bottom</option>
+          </select>
+          <div>
+            <button>Set</button>
+          </div>
+        </form>
+      </div>
+      <div className="button-container">
+        <Button />
+      </div>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div className="tooltip-container">
+      <span className="tooltip">Tooltip</span>
+      <button>Hover Over Me</button>
+    </div>
+  );
+};
+
+export default Button;
